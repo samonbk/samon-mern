1bb40fd13371827d8ebf9e28c02548014eb72234
samonbk <samonee7777@gmail.com>
2024-11-19 16:53:03 +0800

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      api: {
-        target: "http://localhost:5000",
-      },
-    },
-  },
+  build: {
+    outDir: 'dist' // Ensure the output directory is "dist"
+  }
 });
